09708348795cb5220c9b675fb40c6e5c374c1218
= <hmzblhdjahmd@gmail.com>
2018-03-24 08:25:25 +0100

    Setup repo

---
+++ cli.js
@@ -0,0 +1,17 @@
+/*!
+ * Manga,dl
+ * Manga Downloader CLI for many famous manga sites around the globe.
+ *
+ * Author: MizoPro
+ * License: MIT <https://github.com/mizopro/blob/master/LICENSE>
+ */
+
+const dl = require(".");
+const pkg = require("./package.json");
+const program = require("commander");
+
+program
+    .version( pkg.version )
+    .description( pkg.description )
+    .usage( 'Not available yet' )
+    .parse( process.argv );
