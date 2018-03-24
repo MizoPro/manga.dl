0e325322645eb30069f366695cb0ba62ee6a0339
MizoPro <hmzblhdjahmd@gmail.com>
2018-03-24 08:42:05 +0100

    Setup repo

---
+++ index.js
@@ -0,0 +1,9 @@
+/*!
+ * Manga,dl
+ * Manga Downloader for many famous manga sites around the globe.
+ *
+ * Author: MizoPro
+ * License: MIT <https://github.com/mizopro/blob/master/LICENSE>
+ */
+
+const cheerio = require("cheerio");
